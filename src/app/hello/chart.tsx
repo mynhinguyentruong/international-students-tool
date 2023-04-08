d13dfb01ac1a6a88960163af502a3a0c58dee714
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-08 10:56:41 -0400

    testing line chart display

--- src/app/hello/chart.tsx
+++ src/app/hello/chart.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card, Title, BarChart, Subtitle } from "@tremor/react";
+import { Card, Title, BarChart, Subtitle, LineChart } from "@tremor/react";
 import { useState } from "react";
 
 const chartdata = [
@@ -18,14 +18,54 @@ const chartdata = [
     },
 ];
 
+const data = [
+    {
+        year: 1951,
+        "Population growth rate": 1.74,
+    },
+    {
+        year: 1952,
+        "Population growth rate": 1.93,
+    },
+    {
+        year: 1953,
+        "Population growth rate": 1.9,
+    },
+    {
+        year: 1954,
+        "Population growth rate": 1.98,
+    },
+    {
+        year: 1955,
+        "Population growth rate": 2,
+    },
+];
+
 const dataFormatter = (number: number) => {
     return "$ " + Intl.NumberFormat("us").format(number).toString();
 };
 
+const percentageDataFormatter = (number: number) => {
+    return `${Intl.NumberFormat("us").format(number).toString()}%`;
+}
+
 export default function Chart() {
 
-    const [data, setData] = useState(chartdata)
+    // const [data, setData] = useState(chartdata)
     return (
+        <>
+            <Card>
+                <Title>Population growth rate (1951 to 2021)</Title>
+                <LineChart
+                    className="mt-6"
+                    data={data}
+                    index="year"
+                    categories={["Population growth rate"]}
+                    colors={["blue"]}
+                    valueFormatter={percentageDataFormatter}
+                    yAxisWidth={40}
+                />
+            </Card>
         <Card>
             <Title>Number of species threatened with extinction (2021)</Title>
             <Subtitle>
@@ -42,5 +82,6 @@ export default function Chart() {
                 yAxisWidth={48}
             />
         </Card>
+        </>
     );
 }
