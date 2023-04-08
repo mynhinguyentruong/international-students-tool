2a18ce018634e35e471006eadf6ec5aaf3d8ae06
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-08 10:06:47 -0400

    Import Chart Component into page.tsx (recommended way by Nextjs to import a Client component into Server Component

---
+++ src/app/hello/chart.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Card, Title, BarChart, Subtitle } from "@tremor/react";
+import { useState } from "react";
+
+const chartdata = [
+    {
+        name: "Amphibians",
+        "Number of threatened species": 2488,
+    },
+    {
+        name: "Birds",
+        "Number of threatened species": 1445,
+    },
+    {
+        name: "Crustaceans",
+        "Number of threatened species": 743,
+    },
+];
+
+const dataFormatter = (number: number) => {
+    return "$ " + Intl.NumberFormat("us").format(number).toString();
+};
+
+export default function Chart() {
+
+    const [data, setData] = useState(chartdata)
+    return (
+        <Card>
+            <Title>Number of species threatened with extinction (2021)</Title>
+            <Subtitle>
+                The IUCN Red List has assessed only a small share of the total known
+                species in the world.
+            </Subtitle>
+            <BarChart
+                className="mt-6"
+                data={chartdata}
+                index="name"
+                categories={["Number of threatened species"]}
+                colors={["blue"]}
+                valueFormatter={dataFormatter}
+                yAxisWidth={48}
+            />
+        </Card>
+    );
+}
---
+++ src/app/hello/page.tsx
@@ -0,0 +1,9 @@
+import Chart from "@/app/hello/chart";
+
+export default function ChartPage() {
+    return (
+        <main>
+            <Chart />
+        </main>
+    )
+}
