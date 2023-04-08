52d514e5b5a0f2951d1ef7682d7fab0fe14b9473
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-04-08 19:16:09 -0400

    Layout /calendar route

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import Calendar from "@/app/calendar/calendar";
+
+export default function CalendarPage() {
+    return(
+        <main className="w-full p-10 h-full">
+            <Calendar />
+        </main>
+    )
+}
